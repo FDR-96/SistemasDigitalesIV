fa9f45ce8d4ad8809ce0844503935a09dfbf829b
FDR-96 <roldanfede96@gmail.com>
2020-07-18 23:41:43 -0300

    Login y cierre de sesion

--- Android/ControlBogie/app/src/main/java/com/example/controlbogie/MainActivity.java
+++ Android/ControlBogie/app/src/main/java/com/example/controlbogie/MainActivity.java
@@ -1,27 +1,64 @@
 package com.example.controlbogie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private Button  bIniciar;
     private Button bRegistrar;
+    private EditText tEmail;
+    private EditText tPass;
+    private String email = "";
+    private String pass = "";
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        tEmail = (EditText) findViewById(R.id.email);
+        tPass = (EditText) findViewById(R.id.password);
         bIniciar = (Button) findViewById(R.id.botoniniciar);
         bRegistrar = (Button) findViewById(R.id.botonregistrar);
 
         bIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivities(new Intent[]{new Intent( MainActivity.this, MenuActivity.class)});
+                email = tEmail.getText().toString();
+                pass = tPass.getText().toString();
+                if(!email.isEmpty() && !pass.isEmpty()){
+                    loginUser();
+                }else{
+                    Toast.makeText(MainActivity.this, "Porfavor, complete los campos y vuelva a intentarlo.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            private void loginUser() {
+                mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            startActivities(new Intent[]{new Intent( MainActivity.this, MenuActivity.class)});
+                        }else{
+                            Toast.makeText(MainActivity.this, "No se pudo iniciar sesion.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
 
--- Android/ControlBogie/app/src/main/java/com/example/controlbogie/MenuActivity.java
+++ Android/ControlBogie/app/src/main/java/com/example/controlbogie/MenuActivity.java
@@ -2,13 +2,38 @@ package com.example.controlbogie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ToggleButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MenuActivity extends AppCompatActivity {
+    private Button bCerrarSesion;
+    private ToggleButton bApagarEncender;
+    private Button bBaseDeDatos;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        bCerrarSesion = (Button) findViewById(R.id.cerrarSesion);
+        bApagarEncender = (ToggleButton) findViewById(R.id.apagarEncender);
+        bBaseDeDatos = (Button) findViewById(R.id.datosAlmacenados);
+
+        bCerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                startActivities(new Intent[]{new Intent( MenuActivity.this, MainActivity.class)});
+                finish();
+            }
+        });
     }
 }

75271c55aae7c18fec0f7e6265f14a9a745d39d3
FDR-96 <roldanfede96@gmail.com>
2020-07-18 21:41:51 -0300

    Creando Interfaz

--- Android/ControlBogie/app/src/main/java/com/example/controlbogie/MainActivity.java
+++ Android/ControlBogie/app/src/main/java/com/example/controlbogie/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.controlbogie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button  bIniciar;
+    private Button bRegistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bIniciar = (Button) findViewById(R.id.botoniniciar);
+        bRegistrar = (Button) findViewById(R.id.botonregistrar);
+
+        bIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivities(new Intent[]{new Intent( MainActivity.this, MenuActivity.class)});
+            }
+        });
+
+        bRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivities(new Intent[]{new Intent( MainActivity.this, RegActivity.class)});
+            }
+        });
+
+
     }
 }
--- Android/ControlBogie/app/src/main/java/com/example/controlbogie/RegActivity.java
+++ Android/ControlBogie/app/src/main/java/com/example/controlbogie/RegActivity.java
@@ -2,13 +2,38 @@ package com.example.controlbogie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegActivity extends AppCompatActivity {
+
+    private Button  bVolver;
+    private Button bAceptar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
+
+        bAceptar = (Button) findViewById(R.id.botonAceptar);
+        bVolver = (Button) findViewById(R.id.botonVolver);
+
+        bAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivities(new Intent[]{new Intent( RegActivity.this, MenuActivity.class)});
+            }
+        });
+
+        bVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivities(new Intent[]{new Intent( RegActivity.this,  MainActivity.class)});
+            }
+        });
     }
+
+
 }
